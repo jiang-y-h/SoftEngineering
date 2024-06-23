5dc4630a32f61216c0c4907d4cd3063e6033de34
jiang <1144764005@qq.com>
2024-06-23 19:28:19 +0800

    Initial commit

---
+++ lib/KPI.py
@@ -0,0 +1,11 @@
+import datetime
+import threading
+import subprocess
+import time
+from index import sql
+import os
+import json
+
+class KPI():
+    def create():
+        pass
---
+++ route/detectKPI.py
@@ -0,0 +1,55 @@
+from flask import request,render_template,redirect,send_file, send_from_directory,url_for,session,make_response, jsonify
+import time
+from index import app,url
+import json
+import os
+import time,random
+from .login import cklogin
+import threading  # 用于多线程处理
+
+url.append({
+    "title": "KPI异常检测",
+    "href": "/KPI",
+})
+
+# 模拟一个训练函数
+@app.route('/StartTraining', methods=['POST'])
+@cklogin()
+def start_training():
+    train_data = request.form.get('model')
+    print(f"Starting training : {train_data}")
+    
+    try:
+        # 获取当前脚本所在目录
+        current_directory = os.path.dirname(os.path.abspath(__file__))
+        parent_directory = os.path.dirname(current_directory)
+        # 保存当前工作目录
+        original_directory = os.getcwd()
+        # 切换工作目录到 data_preprocess.py 所在目录
+        os.chdir(os.path.join(parent_directory, 'OmniAnomaly'))
+        # 构建相对路径调用 data_preprocess.py
+        os.system(f"python data_preprocess.py {train_data}")
+        os.system(f"python main.py")
+        # 恢复原始工作目录
+        os.chdir(original_directory)
+        
+        return jsonify({'resultCode': '0', 'result': '训练成功'})
+    
+    except Exception as e:
+        return jsonify({'resultCode': '1', 'result': f'训练失败：{str(e)}'})
+
+
+@app.route('/KPI', methods=['GET', 'POST'])
+@cklogin()
+def KPIhome():
+    if request.method == 'POST':
+        selected_model = request.form.get('model')
+        if selected_model:
+            # 开始训练的后台任务
+            thread = threading.Thread(target=start_training, args=(selected_model,))
+            thread.start()
+            return jsonify({'resultCode': '0', 'result': '训练已启动'})
+        else:
+            return jsonify({'resultCode': '1', 'result': '未选择训练集'})
+
+    return render_template('kPI.html')
